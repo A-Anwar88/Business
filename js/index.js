5557ebc3ccd9348755b4a07b52e25a64f5e0e224
unknown <ahmed.a.zaky88@gmail.com>
2020-07-03 20:25:33 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function(){
+   "use strict";
+  $(window).scroll(function(){
+      var top = $(window).scrollTop();
+      if(top>=60){
+          $('nav').addClass('secondary');
+      }else if($('nav').hasClass('secondary')){
+          $('nav').removeClass('secondary');
+      }
+      
+  });
+}); 
+(function ($) {
+    "use strict";
+    
+$('.btn-show-menu-mobile').on('click', function(){
+        $(this).toggleClass('is-active');
+        $('.menu-mobile').slideToggle();
+    });
+
+    var arrowMainMenu = $('.arrow-main-menu-m');
+
+    for(var i=0; i<arrowMainMenu.length; i++){
+        $(arrowMainMenu[i]).on('click', function(){
+            $(this).parent().find('.sub-menu-m').slideToggle();
+            $(this).toggleClass('turn-arrow-main-menu-m');
+        })
+    }
+
+    $(window).resize(function(){
+        if($(window).width() >= 992){
+            if($('.menu-mobile').css('display') == 'block') {
+                $('.menu-mobile').css('display','none');
+                $('.btn-show-menu-mobile').toggleClass('is-active');
+            }
+
+            $('.sub-menu-m').each(function(){
+                if($(this).css('display') == 'block') { console.log('hello');
+                    $(this).css('display','none');
+                    $(arrowMainMenu).removeClass('turn-arrow-main-menu-m');
+                }
+            });
+                
+        }
+    });
+});
